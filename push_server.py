58171a3de50280d1b5c153a12be7673e4e9b0d36
Egill Sigurðarson <egills@pm.me>
2023-02-08 16:28:03 +0000

    Line chart with minimal eventlet backend

--- push_server.py
+++ push_server.py
@@ -1,27 +1,31 @@
 """
 A simple webserver to demonstrate SocketIO workflow. To run install:
 
-  $ pip install flask flask-socketio simple-websocket
+  $ pip install flask flask-socketio eventlet
 
 and run this file. It uses the signal system to generate random data and stream
 via websocket to browser. The sampling rate of the system and the rate at
 which data is streamed can be set independently using the constants SAMPLING_RATE
 and GUI_UPDATE_RATE.
 """
+import datetime
+import sys
+from pathlib import Path
+from threading import Lock
+
+import eventlet
+import numpy as np
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
-from threading import Lock
-import sys
-from pathlib import Path
-
-from signal_processing.data_sources import (
+import signal_processing.signals as s  # noqa: E402
+from signal_processing.data_sources import (  # noqa: E402
     ManualTimerDataSource,
     RandomDataSource,
-)  # noqa: E402
+)
 from signal_processing.system import SignalSystem  # noqa: E402
-import signal_processing.signals as s  # noqa: E402
 
+eventlet.monkey_patch()
 app = Flask(__name__)
 socketio = SocketIO(app)
 
@@ -29,20 +33,21 @@ thread_lock = Lock()
 data_thread = None
 
 SAMPLING_RATE = 100
-GUI_UPDATE_RATE = 100
+GUI_UPDATE_RATE = 50
 
 
 def generate_data():
     source = ManualTimerDataSource(
         SAMPLING_RATE, secondary_sources=[RandomDataSource()]
     )
-    system = SignalSystem(source, derived=[s.SamplingRate()])
+    # source = MicDataSource()
+    system = SignalSystem(source, derived=[])
+    print("called generate_data")
     with system:
         while True:
-            data = system.read(as_dataframe=False)
-            for key, val in data.items():
-                data[key] = val.tolist()
-            socketio.emit("data", data, broadcast=True)
+            data = system.read(as_dataframe=True)
+            data_points = [dict(row) for _, row in data.iterrows()]
+            socketio.emit("data", data_points, broadcast=True)
             socketio.sleep(1 / GUI_UPDATE_RATE)
 
 
@@ -51,14 +56,6 @@ def index():
     return render_template("index.html")
 
 
-@socketio.on("connect")
-def handle_connect():
-    print("client connected")
-    global data_thread
-    with thread_lock:
-        if data_thread is None:
-            data_thread = socketio.start_background_task(generate_data)
-
-
 if __name__ == "__main__":
+    socketio.start_background_task(generate_data)
     socketio.run(app, debug=True)
