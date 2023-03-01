bf11b3ac6ef27c5600bd8d58e225cb0d6abb02b8
BjarniHaukur <bjarnihaukur11@gmail.com>
2023-03-01 16:25:47 +0000

    uncomment and update dashboard for new BasePlot version

--- genki-signals/src/lib/scidashboard/Dashboard.svelte
+++ genki-signals/src/lib/scidashboard/Dashboard.svelte
@@ -13,7 +13,7 @@
 
 	let el: HTMLDivElement;
 
-	const pixel_width = 1000;
+	const pixel_width = 2540;
 	const pixel_height = 1000;
 
 	let main_surface: SciChartSurface, wasm_context: TSciChart;
@@ -28,8 +28,8 @@
 			main_surface = resolve.sciChartSurface;
 			wasm_context = resolve.wasmContext;
 
-			const num_graphs = 4;
-			const num_columns = 2;
+			const num_graphs = 1;
+			const num_columns = 1;
 			const rects = getSubChartRects(
 				num_graphs,
 				1 / (num_graphs / num_columns),
@@ -38,15 +38,17 @@
 			);
 			const line_options = get_default_line_plot_options();
 			line_options.auto_range = true;
-			line_options.x_axis_flipped = true;
+			line_options.x_axis_flipped = false;
 			line_options.x_axis_visible = false;
+			line_options.y_axis_visible = false;
+			line_options.y_axis_flipped = true;
 			line_options.data_is_sorted = true;
 			line_options.data_contains_nan = false;
-			line_options.n_visible_points = 100;
+			line_options.n_visible_points = 1000;
+			line_options.sig_x = [{ sig_name: 'timestamp_us', sig_idx: 0 }];
 			line_options.sig_y = [
 				{ sig_name: 'random', sig_idx: 0 },
-				{ sig_name: 'mouse_pos', sig_idx: 0 },
-				{ sig_name: 'mouse_pos', sig_idx: 1 }
+				// { sig_name: 'random', sig_idx: 1 }
 			];
 
 			const subcharts = Array(num_graphs)
