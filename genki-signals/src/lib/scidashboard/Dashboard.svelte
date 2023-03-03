875f62af83ad75c283928baf957459747fa05cb9
BjarniHaukur <bjarnihaukur11@gmail.com>
2023-03-03 13:12:52 +0000

    small change for demo

--- genki-signals/src/lib/scidashboard/Dashboard.svelte
+++ genki-signals/src/lib/scidashboard/Dashboard.svelte
@@ -6,15 +6,40 @@
 
 	import { getSubChartRects } from '../utils/subchart_helpers';
 	import { SubChart } from '../scicharts/subchart';
-	import { get_default_line_plot_options } from '../scicharts/line';
+	import { get_default_line_plot_options, type LinePlotOptions } from '../scicharts/line';
 	import { SCICHART_KEY } from '../utils/constants';
 
 	const socket = io('http://localhost:5000');
 
 	let el: HTMLDivElement;
 
-	const pixel_width = 1000;
+	const pixel_width = 1440;
 	const pixel_height = 1000;
+
+	const num_charts = 2;
+	const num_columns = 1;
+
+	const line_options: LinePlotOptions = {
+		type: 'line', // only line for now
+        sig_x: [{ sig_name: 'timestamp_us', sig_idx: 0 }],
+        sig_y: [
+			{ sig_name: 'random', sig_idx: 0 },
+			// { sig_name: 'mouse_pos', sig_idx: 0 },
+			// { sig_name: 'mouse_pos', sig_idx: 1 },
+		],
+		x_axis_align: 'bottom',
+		y_axis_align: 'left',
+		x_axis_flipped: false,
+		y_axis_flipped: false,
+		x_axis_visible: false,
+		y_axis_visible: true,
+		data_contains_nan: false,
+		data_is_sorted: false,
+		auto_range: false,
+		y_domain_max: 1,
+		y_domain_min: 0,
+		n_visible_points: 100,
+	};
 
 	let main_surface: SciChartSurface, wasm_context: TSciChart;
 	onMount(() => {
@@ -28,30 +53,16 @@
 			main_surface = resolve.sciChartSurface;
 			wasm_context = resolve.wasmContext;
 
-			const num_graphs = 1;
-			const num_columns = 1;
+
 			const rects = getSubChartRects(
-				num_graphs,
-				1 / (num_graphs / num_columns),
+				num_charts,
+				1 / (num_charts / num_columns),
 				1 / num_columns,
 				num_columns
 			);
-			const line_options = get_default_line_plot_options();
-			line_options.auto_range = true;
-			line_options.x_axis_flipped = false;
-			line_options.x_axis_visible = false;
-			line_options.y_axis_visible = false;
-			line_options.y_axis_flipped = true;
-			line_options.data_is_sorted = true;
-			line_options.data_contains_nan = false;
-			line_options.n_visible_points = 1000;
-			line_options.sig_x = [{ sig_name: 'timestamp_us', sig_idx: 0 }];
-			line_options.sig_y = [
-				{ sig_name: 'random', sig_idx: 0 },
-				// { sig_name: 'random', sig_idx: 1 }
-			];
 
-			const subcharts = Array(num_graphs)
+
+			const subcharts = Array(num_charts)
 				.fill(0)
 				.map((_, i) => new SubChart('bla', main_surface, wasm_context, rects[i], line_options));
 
