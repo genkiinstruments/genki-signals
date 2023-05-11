e97d312eccad4edc80bc1ed9afbb3f3e85d03e26
BjarniHaukur <bjarnihaukur11@gmail.com>
2023-05-11 16:36:56 +0000

    better tests and code

--- genki_signals/signal_functions/shape.py
+++ genki_signals/signal_functions/shape.py
@@ -6,13 +6,19 @@ from genki_signals.signal_functions.base import SignalFunction, SignalName
 
 
 class ExtractDimension(SignalFunction):
-    """ SignalFunction to extract a dimension from a signal """
+    """SignalFunction to extract a dimension from a signal"""
 
     def __init__(self, input_signal: SignalName, name: str, dim: int | tuple[int]):
         super().__init__(input_signal, name=name, params={"dim": dim})
+        if isinstance(dim, int):
+            dim = (dim,)
+
         self.dim = dim
 
     def __call__(self, v):
+        if v.ndim == len(self.dim):
+            raise ValueError(f"Cannot not convert signal to scalar")
+
         return v[self.dim]
 
 
@@ -30,6 +36,11 @@ class Concatenate(SignalFunction):
                 to_concat.append(col_data[None])
             else:
                 to_concat.append(col_data)
+
+        ndim = to_concat[0].ndim
+        if self.axis in [-1, ndim - 1]:
+            raise ValueError(f"Cannot concatenate along time axis")
+
         return np.concatenate(to_concat, axis=self.axis)
 
 
@@ -41,16 +52,24 @@ class Stack(SignalFunction):
         self.axis = axis
 
     def __call__(self, *signals):
-        return np.stack(signals, axis=self.axis)
+        stacked = np.stack(signals, axis=self.axis)
+
+        ndim = stacked.ndim
+        if self.axis in [-1, ndim - 1]:
+            raise ValueError(f"Cannot stack along time axis")
+
+        return stacked
 
 
 class Reshape(SignalFunction):
+    """SignalFunction to reshape a signal without changing the time axis"""
+
     def __init__(self, input_signal: SignalName, shape: tuple[int], name: str):
         super().__init__(input_signal, name=name, params={"shape": shape})
         self.shape = shape
 
     def __call__(self, v):
-        time = v.shape[-1] # time axis should not be reshaped
+        time = v.shape[-1]
         return v.reshape(self.shape + (time,))
 
 
--- tests/signal_functions/test_shape.py
+++ tests/signal_functions/test_shape.py
@@ -7,11 +7,11 @@ from genki_signals.signal_functions.shape import *
 @pytest.mark.parametrize(
     "dim, input_data, expected",
     [
-        (0, np.array([[1,2], [3,4]]), np.array([1,2])),
-        (1, np.array([[1,2], [3,4]]), np.array([3,4])),
-        (0, np.array([[[1,2], [3,4]], [[5,6], [7,8]]]), np.array([[1,2], [3,4]])),
-        ((0,0), np.array([[[1,2], [3,4]], [[5,6], [7,8]]]), np.array([1,2])),
-    ]
+        (0, np.array([[1, 2], [3, 4]]), np.array([1, 2])),
+        (1, np.array([[1, 2], [3, 4]]), np.array([3, 4])),
+        (0, np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]]), np.array([[1, 2], [3, 4]])),
+        ((0, 0), np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]]), np.array([1, 2])),
+    ],
 )
 def test_extract_dimension(input_data, dim, expected):
     extract_dim = ExtractDimension("input_signal", name="ExtractDimension", dim=dim)
@@ -20,14 +20,25 @@ def test_extract_dimension(input_data, dim, expected):
 
 
 @pytest.mark.parametrize(
+    "dim, input_data",
+    [
+        (0, np.array([1, 2])),
+        ((0, 0), np.array([[1, 2]])),
+    ],
+)
+def test_extract_dimension_shape_mismatch(dim, input_data):
+    extract_dim = ExtractDimension("input_signal", name="ExtractDimension", dim=dim)
+    with pytest.raises(ValueError):
+        extract_dim(input_data)
+
+
+@pytest.mark.parametrize(
     "axis, input_data, expected",
     [
-        (0, [np.array([1,2]), np.array([3,4])], np.array([[1,2],[3,4]])),
-        (0, [np.array([[1,2]]), np.array([[3,4]])], np.array([[1,2], [3,4]])),
-        (1, [np.array([1,2]), np.array([3,4])], np.array([[1,2,3,4]])),
-        (1, [np.array([[1,2]]), np.array([[3,4]])], np.array([[1,2,3,4]])),
-        (0, [np.array([[1,2], [3,4]]), np.array([[5,6]])], np.array([[1,2], [3,4], [5,6]])),
-    ]
+        (0, [np.array([1, 2]), np.array([3, 4])], np.array([[1, 2], [3, 4]])),
+        (0, [np.array([[1, 2]]), np.array([[3, 4]])], np.array([[1, 2], [3, 4]])),
+        (0, [np.array([[1, 2], [3, 4]]), np.array([[5, 6]])], np.array([[1, 2], [3, 4], [5, 6]])),
+    ],
 )
 def test_concatenate(axis, input_data, expected):
     concatenate = Concatenate("input_signal", name="Concatenate", axis=axis)
@@ -38,9 +49,11 @@ def test_concatenate(axis, input_data, expected):
 @pytest.mark.parametrize(
     "axis, input_data",
     [
-        (0, [np.array([[1,2,3]]), np.array([[4,5]])]),
-        (0, [np.array([[1,2], [3,4]]), np.array([[5,6,7]])]),
-    ]
+        (0, [np.array([[1, 2, 3]]), np.array([[4, 5]])]),
+        (0, [np.array([[1, 2], [3, 4]]), np.array([[5, 6, 7]])]),
+        (1, [np.array([1, 2]), np.array([3, 4])]),
+        (1, [np.array([[1, 2]]), np.array([[3, 4]])]),
+    ],
 )
 def test_concatenate_shape_mismatch(axis, input_data):
     concatenate = Concatenate("input_signal", name="Concatenate", axis=axis)
@@ -51,25 +64,41 @@ def test_concatenate_shape_mismatch(axis, input_data):
 @pytest.mark.parametrize(
     "axis, input_data, expected",
     [
-        (0, [np.array([1,2]), np.array([3,4])], np.array([[1,2],[3,4]])),
-        (0, [np.array([[1,2]]), np.array([[3,4]])], np.array([[[1,2]], [[3,4]]])),
-        (1, [np.array([[1,2]]), np.array([[3,4]])], np.array([[[1,2], [3,4]]])),
-    ]
+        (0, [np.array([1, 2]), np.array([3, 4])], np.array([[1, 2], [3, 4]])),
+        (0, [np.array([[1], [2]]), np.array([[3], [4]])], np.array([[[1], [2]], [[3], [4]]])),
+        (1, [np.array([[1], [2]]), np.array([[3], [4]])], np.array([[[1], [3]], [[2], [4]]])),
+        (0, [np.array([[1, 2]]), np.array([[3, 4]])], np.array([[[1, 2]], [[3, 4]]])),
+        (1, [np.array([[1, 2]]), np.array([[3, 4]])], np.array([[[1, 2], [3, 4]]])),
+    ],
 )
 def test_stack(axis, input_data, expected):
     stack = Stack("input_signal", name="Stack", axis=axis)
     result = stack(*input_data)
     np.testing.assert_array_equal(result, expected)
-    
+
+
+@pytest.mark.parametrize(
+    "axis, input_data",
+    [
+        (1, [np.array([1, 2]), np.array([3, 4])]),
+        (-1, [np.array([1, 2]), np.array([3, 4])]),
+        (2, [np.array([[1, 2]]), np.array([[3, 4]])]),
+    ],
+)
+def test_stack_time_axis(axis, input_data):
+    stack = Stack("input_signal", name="Stack", axis=axis)
+    with pytest.raises(ValueError):
+        stack(*input_data)
+
 
 @pytest.mark.parametrize(
     "shape, input_data, expected",
     [
-        ((2,2), np.arange(40).reshape((4, 10)), np.arange(40).reshape((2, 2, 10))),
+        ((2, 2), np.arange(40).reshape((4, 10)), np.arange(40).reshape((2, 2, 10))),
         ((4,), np.arange(40).reshape((4, 10)), np.arange(40).reshape((4, 10))),
         ((4,), np.arange(40).reshape((2, 2, 10)), np.arange(40).reshape((4, 10))),
         ((-1,), np.arange(40).reshape((2, 2, 10)), np.arange(40).reshape((4, 10))),
-    ]
+    ],
 )
 def test_reshape(shape, input_data, expected):
     reshape = Reshape("input_signal", name="Reshape", shape=shape)
@@ -81,8 +110,8 @@ def test_reshape(shape, input_data, expected):
     "shape, input_data",
     [
         ((5,), np.arange(40).reshape((4, 10))),
-        ((4,2), np.arange(40).reshape((2, 2, 10))),
-    ]
+        ((4, 2), np.arange(40).reshape((2, 2, 10))),
+    ],
 )
 def test_reshape_shape_mismatch(shape, input_data):
     reshape = Reshape("input_signal", name="Reshape", shape=shape)
