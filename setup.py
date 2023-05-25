19b6ad758ab9637c7373bdafbe97afe0e54b635c
Egill Sigurðarson <egills@pm.me>
2023-05-25 11:35:30 +0000

    Also exclude test in setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setuptools.setup(
     author="Genki Instruments",
     author_email="genki@genkiinstruments.com",
     keywords = ["Signal Processing", "Machine Learning", "Realtime"],
-    packages=setuptools.find_packages(exclude=("tests", "examples")),
+    packages=setuptools.find_packages(exclude=("tests", "test", "examples")),
     classifiers = [
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
